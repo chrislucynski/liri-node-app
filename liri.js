d4d439edfac3472c4b49679c090cf1d6fb223b4f
Chris Lucynski <chris.lucynski@gmail.com>
2019-09-11 18:10:56 -0600

    added code for concert this

--- liri.js
+++ liri.js
@@ -1,19 +1,31 @@
 require("dotenv").config();
+
 var axios = require('axios');
 var commandInput = process.argv.slice(2).join(' ')
-
+var moment = require('moment')
 
 // Concert Stuff
-var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+var concertURL = "https://rest.bandsintown.com/artists/" + commandInput + "/events?app_id=codingbootcamp"
 
 function concertThis(){
-axios.get(concertURL).then(function(data){
-console.log(data)
-var venueName
-var venueLocation
-var eventDate
-}
+    axios.get(concertURL).then(function(data){
+        var concertData = data.data
+        for(var i = 0; i < concertData.length; i++){
+            // console.log(concertData)
+            var venueName = concertData[i].venue.name
+            var venueLocation = concertData[i].venue.city + ", " + concertData[i].venue.country
+            var eventDate = concertData[i].datetime
+            eventDate = moment(eventDate).format('MM/DD/YYYY'); 
+            console.log(venueName)
+            console.log(venueLocation)
+            console.log(eventDate)
+            console.log('<--------------------->')
 
+        }
+    })
+    .catch(error => console.log(error));
+}
+concertThis();
 
 // Spotify Stuff
 var Spotify = require('node-spotify-api');
@@ -29,13 +41,11 @@ function spotifyThisSong(){
         console.log('Sorry, no song by that name is available.  Pick a different song.')    
         return        
       }
-     
         var songObj = data.tracks.items[0]
         var songName = songObj.name
         var songArtist = songObj.artists[0].name
         var songAlbum = songObj.album.name
         var songPreviewUrl = songObj.preview_url
-    
         console.log('Song Name: ' + songName); 
         console.log('Song Artist: ' + songArtist); 
         console.log('Song Album: ' + songAlbum); 
@@ -46,8 +56,7 @@ function spotifyThisSong(){
 }
 // spotifyThisSong();
 
-
-
+// <-------------------------> 
 // OMDB Stuff
 function movieThis(){
     if(!commandInput){
@@ -74,15 +83,14 @@ function movieThis(){
         console.log('Plot: ' +moviePlot)
         console.log('Actors: ' +movieActor)
     })
-
-};
+}
 // movieThis();
 
 
 
 // Do What It Says Stuff
 
-var fs = require('fs');
+// var fs = require('fs');
 
 
 
