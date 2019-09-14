da99cf7031c5b0b618dd2ae340f04bdc3faf9d0f
Chris Lucynski <chris.lucynski@gmail.com>
2019-09-14 14:50:02 -0600

    fixed do-what-it-says and added extra function

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ LIRI will be a command line node app that takes in parameters and gives you back
 
 This node-based app is intended to allow the user to input commands, and return entertainment data of their choosing.  The 4 different commands the user can run are:
 
-1) 'spotify-this-song' - this command along with a song title will retrieve and display data (artist(s), song's name, preview link of the song, and the album the song is from) from Spotify.
+1) `spotify-this-song` - this command along with a song title will retrieve and display data (artist(s), song's name, preview link of the song, and the album the song is from) from Spotify.
 
-2) 'concert-this' - this command along with an artist's name will retrieve and display data (venue name, venue location, date of event) from Bands in Town Artist Events API.
+2) `concert-this` - this command along with an artist's name will retrieve and display data (venue name, venue location, date of event) from Bands in Town Artist Events API.
 
-3) 'movie-this' - this command along with an artist's name will retrieve and display data (movie title, release year, IMDB movie rating, Rotten Tomatoes movie rating, country where the movie was produced, language of the movie, movie plot, actors/actresses) from OMDB.
+3) `movie-this` - this command along with an artist's name will retrieve and display data (movie title, release year, IMDB movie rating, Rotten Tomatoes movie rating, country where the movie was produced, language of the movie, movie plot, actors/actresses) from OMDB.
 
-4) 'do-what-it-says' - this command will read the contents in the 'random.txt' file, which should contain one of the aforementioned commands and the desired song, artist, or movie. 
+4) `do-what-it-says` - this command will read the contents in the `random.txt` file, which should contain one of the aforementioned commands and the desired song, artist, or movie. 
 
 ### Give a high-level overview of how the app is organized
 
@@ -19,15 +19,20 @@ The app contains functions to access information from different API's in order t
 
 ### Give start-to-finish instructions on how to run the app
 
-To begin using the app, use one of the aforementioned commands and the input required for each command:
+To use the app, the user will first need to navigate to the project's folder. Once completed, the user will need to install a few packages to ensure the app runs correctly. If the user runs `npm install`, it will install all necessary packages because of dependencies. 
 
-If you are using the 'spotify-this-song' command, type in the name of the song.
+After the packages have been installed, the user is ready to run commands from the app.  In order for them to run correctly, they must be entered in the following format: `node <file-name> <command-name> <user-input>`
 
-if you are using the 'concert-this' command, type in the name of the artist.
+For the user input, use the following instructions:
 
-if you are using the 'movie-this' command, type in the name of the movie.
+If you are using the `spotify-this-song` command, type in the name of the song.
+If you are using the `concert-this` command, type in the name of the artist.
+If you are using the `movie-this` command, type in the name of the movie.
+If you are using the `do-what-it-says` command, you will not need to type in additional input.
 
-using the 'do-what-it-says' command, you will not need to type in additional input.
+See below for an example of the correct format for running a command:
+
+`node liri.js movie-this The Matrix`
 
 ### Include screenshots, gifs or videos of the app functioning
 
--- liri.js
+++ liri.js
@@ -1,137 +1,150 @@
 require("dotenv").config();
 
-var axios = require('axios');
-var commandInput = process.argv.slice(3).join(' ')
-var command = process.argv[2]
+var axios = require("axios");
+var commandInput = process.argv.slice(3).join(" ");
+var command = process.argv[2];
 // console.log(command)
-var moment = require('moment')
+var moment = require("moment");
 
 // Concert Stuff
-var concertURL = "https://rest.bandsintown.com/artists/" + commandInput + "/events?app_id=codingbootcamp"
 
-function concertThis(){
-    axios.get(concertURL).then(function(data){
-        var concertData = data.data
-        for(var i = 0; i < concertData.length; i++){
-            // console.log(concertData)
-            var venueName = concertData[i].venue.name
-            var venueLocation = concertData[i].venue.city + ", " + concertData[i].venue.country
-            var eventDate = concertData[i].datetime
-            eventDate = moment(eventDate).format('MM/DD/YYYY'); 
-            console.log(venueName)
-            console.log(venueLocation)
-            console.log(eventDate)
-            console.log('<--------------------->')
-        }
+function concertThis(artist) {
+  console.log(artist);
+  var concertURL =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+  axios
+    .get(concertURL)
+    .then(function(data) {
+      var concertData = data.data;
+      for (var i = 0; i < concertData.length; i++) {
+        // console.log(concertData)
+        var venueName = concertData[i].venue.name;
+        var venueLocation =
+          concertData[i].venue.city + ", " + concertData[i].venue.country;
+        var eventDate = concertData[i].datetime;
+        eventDate = moment(eventDate).format("MM/DD/YYYY");
+        console.log(venueName);
+        console.log(venueLocation);
+        console.log(eventDate);
+        console.log("<--------------------->");
+      }
     })
     .catch(error => console.log(error));
 }
 // concertThis();
 
-// <-------------------------> 
+// <------------------------->
 
 // Spotify Stuff
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 
 var spotify = new Spotify({
-    id: process.env.spotify_client_ID,
-    secret: process.env.spotify_client_secret_ID
+  id: process.env.spotify_client_ID,
+  secret: process.env.spotify_client_secret_ID
 });
-   
-function spotifyThisSong(){
-    spotify.search({ type: 'track', query: commandInput, limit: 1}, function(err, data) {
-        var songObj = data.tracks.items[0]
-        var songName = songObj.name
-        var songArtist = songObj.artists[0].name
-        var songAlbum = songObj.album.name
-        var songPreviewUrl = songObj.preview_url
-        console.log('Song Name: ' + songName); 
-        console.log('Song Artist: ' + songArtist); 
-        console.log('Song Album: ' + songAlbum); 
-        console.log('Song Preview: ' + songPreviewUrl); 
-        if(songName === undefined){
-            console.log('Sorry, no song by that name is available.  Pick a different song.')    
-        }
-        if (err) {
-            console.log(err)
-            return        
-        }
-    });
+
+function spotifyThisSong(song) {
+  spotify.search({ type: "track", query: song, limit: 1 }, function(err, data) {
+    var songObj = data.tracks.items[0];
+    var songName = songObj.name;
+    var songArtist = songObj.artists[0].name;
+    var songAlbum = songObj.album.name;
+    var songPreviewUrl = songObj.preview_url;
+    console.log("Song Name: " + songName);
+    console.log("Song Artist: " + songArtist);
+    console.log("Song Album: " + songAlbum);
+    console.log("Song Preview: " + songPreviewUrl);
+    if (songName === undefined) {
+      console.log(
+        "Sorry, no song by that name is available.  Pick a different song."
+      );
+    }
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
 }
 // spotifyThisSong();
 
-// <-------------------------> 
+// <------------------------->
 
 // OMDB Stuff
-function movieThis(){
-    if(!commandInput){
-        commandInput = 'Mr. Nobody'
-    }
-    console.log(commandInput)
-    var queryURL = 'http://www.omdbapi.com/?&t=' + commandInput + '&apikey=trilogy';
-    axios.get(queryURL).then(function(data){
-        var movieData = data.data
-        var movieTitle = movieData.Title
-        var movieYear = movieData.Year
-        var movieIMDBRatings = movieData.Ratings[0].Value
-        var movieTomatoRating = movieData.Ratings[1].Value
-        var movieLanguage = movieData.Language
-        var movieCountry = movieData.Country
-        var moviePlot = movieData.Plot
-        var movieActor = movieData.Actors
-        console.log('Movie Title: ' +movieTitle)
-        console.log('Release Year: ' +movieYear)
-        console.log('IMDB Rating: ' +movieIMDBRatings)
-        console.log('Rotten Tomatoes Rating: ' +movieTomatoRating)
-        console.log('Language: ' +movieLanguage)
-        console.log('Country: ' +movieCountry)
-        console.log('Plot: ' +moviePlot)
-        console.log('Actors: ' +movieActor)
-    })
+function movieThis(movie) {
+  if (!movie) {
+    movie = "Mr. Nobody";
+  }
+  console.log(movie);
+  var queryURL = "http://www.omdbapi.com/?&t=" + movie + "&apikey=trilogy";
+  axios.get(queryURL).then(function(data) {
+    var movieData = data.data;
+    var movieTitle = movieData.Title;
+    var movieYear = movieData.Year;
+    var movieIMDBRatings = movieData.Ratings[0].Value;
+    var movieTomatoRating = movieData.Ratings[1].Value;
+    var movieLanguage = movieData.Language;
+    var movieCountry = movieData.Country;
+    var moviePlot = movieData.Plot;
+    var movieActor = movieData.Actors;
+    console.log("Movie Title: " + movieTitle);
+    console.log("Release Year: " + movieYear);
+    console.log("IMDB Rating: " + movieIMDBRatings);
+    console.log("Rotten Tomatoes Rating: " + movieTomatoRating);
+    console.log("Language: " + movieLanguage);
+    console.log("Country: " + movieCountry);
+    console.log("Plot: " + moviePlot);
+    console.log("Actors: " + movieActor);
+  });
 }
 // movieThis();
 
-// <-------------------------> 
+// <------------------------->
 
 // Do What It Says Stuff
 
-var fs = require('fs');
-
-// fs.writeFile('random.txt', commandInput, function (err) {
-//     if (err) throw err;
-//     console.log('Written!');
-//   });
+var fs = require("fs");
 
 // fs.appendFile('log.txt', command + ' ' + commandInput + ', ', 'utf8', function (err) {
 //     if (err) throw err;
 //     console.log('Logged!');
 //   });
 
-function doThis(){
-    fs.readFile('random.txt', 'utf8', function (err, data) {
-        if (err) throw err;
-        var randomTextArr = data.split(',')
-        command = randomTextArr[0]
-        commandInput = randomTextArr[1]
-        console.log(command, commandInput)
-    });
+function doThis() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) throw err;
+    var randomTextArr = data.split(",");
+    command = randomTextArr[0];
+    commandInput = randomTextArr[1];
+    switchFunction(command, commandInput);
+    console.log("do-this function running");
+  });
 }
 
-switch (command){
-    case 'spotify-this-song':
-        spotifyThisSong()
-        break;
-    case 'concert-this':
-        concertThis();
-        break;
-    case 'movie-this':
-        movieThis();
-        break;
-    case 'do-what-it-says':
-        doThis();
-        break;
+function switchFunction(command, commandInput) {
+  switch (command) {
+    case "spotify-this-song":
+      console.log("spotify-this-song is targeted");
+      spotifyThisSong(commandInput);
+      return true;
+    case "concert-this":
+      console.log("concert-this is targeted");
+      concertThis(commandInput);
+      return true;
+    case "movie-this":
+      console.log("movie-this is targeted");
+      movieThis(commandInput);
+      return true;
+  }
+  return false;
 }
 
+function switchFunctionAll(command, commandInput) {
+  if (!switchFunction(command, commandInput) && command === "do-what-it-says") {
+    console.log("switch-function-all");
+    doThis();
+  }
+}
 
-
-
+switchFunctionAll(command, commandInput);
